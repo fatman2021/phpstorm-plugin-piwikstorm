26dfaaa40cdf822a52fdbe9b91c2ba4658faf71d
diosmosis <benaka@piwik.pro>
2015-02-15 06:31:20 -0800

    Add resources bundle for inspection description, add script to run command line inspect + inspection profile for use on travis.

---
+++ misc/run_inspect.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+# usage: ./run_inspect.sh /path/to/phpstorm/root /path/to/piwik/root PluginName
+
+PHPSTORM_PATH="$1"
+PIWIK_PATH="$2"
+PLUGIN_NAME="$3"
+
+SCRIPT_DIR=`dirname $0`
+INSPECTION_PROFILE_PATH="$SCRIPT_DIR/Plugin_Quality_Checks.xml"
+OUTPUT_PATH="$SCRIPT_DIR/output"
+
+mkdir -p "$OUTPUT_PATH"
+
+"$PHPSTORM_PATH/bin/inspect.sh" "$PIWIK_PATH" "$INSPECTION_PROFILE_PATH" "$OUTPUT_PATH" -d "$PIWIK_PATH/plugins/$PLUGIN_NAME" -v2

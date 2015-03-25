41d48e3cd674bc4893b753ff0444fdca7c701e7c
diosmosis <benaka@piwik.pro>
2015-03-24 21:37:58 -0700

    Get tests to run successfully in Intellij IDEA Ultimate (tests fail, but run).

--- test/com/piwik/intellijplugins/piwikstorm/inspections/tests/PiwikNonApiInspectionTest.java
+++ test/com/piwik/intellijplugins/piwikstorm/inspections/tests/PiwikNonApiInspectionTest.java
@@ -1,13 +1,16 @@
 package com.piwik.intellijplugins.piwikstorm.inspections.tests;
 
 import com.intellij.codeInspection.LocalInspectionTool;
+import com.intellij.openapi.application.ex.PathManagerEx;
 import com.intellij.openapi.vfs.LocalFileSystem;
 import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.testFramework.InspectionTestCase;
 import com.intellij.testFramework.PsiTestUtil;
 import com.piwik.intellijplugins.piwikstorm.inspections.PiwikNonApiInspection;
+import org.jetbrains.annotations.NonNls;
 
 import java.io.File;
+import java.net.URL;
 
 public class PiwikNonApiInspectionTest extends InspectionTestCase
 {
@@ -26,12 +29,28 @@ public class PiwikNonApiInspectionTest extends InspectionTestCase
     protected void setupRootModel(String testDir, VirtualFile[] sourceDir, String sdkName) {
         super.setupRootModel(testDir, sourceDir, sdkName);
 
-        File mockPiwikCodePath = new File(testDir + "../../../mockPiwikCode");
+        File mockPiwikCodePath = new File(testDir + "/../../../mockPiwikCode");
         VirtualFile mockPiwikCode =  LocalFileSystem.getInstance().refreshAndFindFileByIoFile(mockPiwikCodePath);
         if (mockPiwikCode == null) {
             assertNotNull("could not find mockPiwikCode dir at " + mockPiwikCodePath, mockPiwikCode);
         }
 
         PsiTestUtil.addSourceRoot(this.myModule, mockPiwikCode);
+    }
+
+    @NonNls
+    protected String getTestDataPath() {
+        ClassLoader thisClassLoader = PiwikNonApiInspectionTest.class.getClassLoader();
+        URL resourceUrl = PiwikNonApiInspectionTest.class.getResource("/mockPiwikCode/PiwikCode.php");
+
+        String testDataPath;
+        try {
+            File resourcePath = new File(resourceUrl.toURI());
+            testDataPath = resourcePath.getParentFile().getParentFile().toString();
+        } catch (Exception ex) {
+            testDataPath = PathManagerEx.getTestDataPath();
+        }
+
+        return testDataPath + "/inspection/";
     }
 }
